5e0d357602b4b7a6507476c335c104bb515e94e0
Jorge Vergara <javebratt@gmail.com>
2017-01-24 11:15:24 -0500

    Added whitespace to the imports

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {TranslateService} from 'ng2-translate';
+import { TranslateService } from 'ng2-translate';
 import { NavController } from 'ionic-angular';
 
 @Component({
